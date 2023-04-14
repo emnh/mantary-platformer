8c5367e3ec1b65362bd0c4260e66a58dce5ff130
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-14 11:13:57 +0200

    update

--- mantary/main.js
+++ mantary/main.js
@@ -1,13 +1,32 @@
-import './style.css'
-
+import './style.css';
 import interact from 'interact.js';
 import protagonistImgSrc from './images/protagonist.png';
+import locations from './locations.json';
 
 let jsonTextarea;
 
 const images = {
-  protagonist: { 
-    src: protagonistImgSrc
+  protagonist: {
+    src: protagonistImgSrc,
+    width: 100,
+    element: null
+  }
+};
+
+const importJSON = function(json) {
+  const imageData = json;
+
+  for (let i = 0; i < imageData.length; i++) {
+    const data = imageData[i];
+    const imageInfo = images[data.name];
+
+    if (imageInfo && imageInfo.element) {
+      const imageDiv = imageInfo.element;
+      const x = parseFloat(data.x) || 0;
+      const y = parseFloat(data.y) || 0;
+      console.log(x, y);
+      imageDiv.style.transform = `translate(${x}px, ${y}px)`;
+    }
   }
 };
 
@@ -20,32 +39,40 @@ const exportJSON = function() {
     document.body.appendChild(jsonTextarea);
   }
 
-  const images = document.getElementsByTagName('img');
   const imageData = [];
 
-  for (let i = 0; i < images.length; i++) {
-    const image = images[i];
-    const rect = image.getBoundingClientRect();
-    const x = rect.x + window.pageXOffset;
-    const y = rect.y + window.pageYOffset;
-    imageData.push({ src: image.src, x: x, y: y });
+  for (const imageName in images) {
+    if (images.hasOwnProperty(imageName)) {
+      const imageInfo = images[imageName];
+      const rect = imageInfo.element.getBoundingClientRect();
+      const x = rect.x + window.pageXOffset;
+      const y = rect.y + window.pageYOffset;
+      imageData.push({ name: imageName, src: imageInfo.src, x: x, y: y });
+    }
   }
 
   jsonTextarea.value = JSON.stringify(imageData, null, 2);
   // Set the size of the textarea to the size of the text
   jsonTextarea.style.height = Math.min(800, jsonTextarea.scrollHeight) + 'px';
+  // Set the width also
+  jsonTextarea.style.width = 400 + 'px';
 };
 
-const addProtagonist = function() {
-  const protagonistImg = new Image();
-  protagonistImg.src = protagonistImgSrc;
-  protagonistImg.width = 100;
+const addImage = function(imageName) {
+  const imageInfo = images[imageName];
 
-  const protagonistDiv = document.createElement('div');
-  protagonistDiv.appendChild(protagonistImg);
-  document.body.appendChild(protagonistDiv);
+  const image = new Image();
+  image.src = imageInfo.src;
+  image.width = imageInfo.width;
 
-  interact(protagonistDiv).draggable({
+  const imageDiv = document.createElement('div');
+  // imageDiv.style.position = 'absolute';
+  imageDiv.appendChild(image);
+  document.body.appendChild(imageDiv);
+
+  imageInfo.element = imageDiv;
+
+  interact(imageDiv).draggable({
     onmove: function (event) {
       const target = event.target;
       const x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
@@ -59,7 +86,12 @@ const addProtagonist = function() {
 };
 
 const main = function() {
-  addProtagonist();
+  for (const imageName in images) {
+    if (images.hasOwnProperty(imageName)) {
+      addImage(imageName);
+    }
+  }
+  importJSON(locations);
   exportJSON();
 };
 

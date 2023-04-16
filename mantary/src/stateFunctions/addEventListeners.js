490c474d28f31a0139ce796b4e4139bce655ff13
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-16 14:44:33 +0200

    update

--- mantary/src/stateFunctions/addEventListeners.js
+++ mantary/src/stateFunctions/addEventListeners.js
@@ -1,10 +1,10 @@
-function addEventListeners(keysPressed) {
-    document.addEventListener('keydown', function (event) {
+function addEventListeners(docInterface, keysPressed) {
+    docInterface.addEventListener('keydown', function (event) {
         keysPressed[event.key] = true;
         // console.log(event.key);
     });
 
-    document.addEventListener('keyup', function (event) {
+    docInterface.addEventListener('keyup', function (event) {
         delete keysPressed[event.key];
     });
 }

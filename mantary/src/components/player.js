be0034edeeb09306a4f075ce14ab34f1b8b70ea5
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-16 22:13:25 +0200

    update

--- mantary/src/components/player.js
+++ mantary/src/components/player.js
@@ -10,7 +10,7 @@ export function Player(f) {
     } = f;
 
     let state = {
-        gameSpeedInMSPerTick: 100,
+        gameSpeedInMSPerTick: 10,
         moveSpeed: 0.0001,
         movementVector: { x: 0, y: 0, },
         worldPosition: { x: 0, y: 0, },
@@ -19,7 +19,9 @@ export function Player(f) {
 
     let raf = null;
     let rafCallbacks = {};
-    let lastTime = null;
+    let engineCallbacks = {};
+    let lastRafTime = null;
+    let lastEngineTime = null;
     let running = false;
     let keysPressed = {};
     let lastKeysPressed = {};
@@ -39,17 +41,24 @@ export function Player(f) {
         rafCallbacks[name] = raf;
     }
 
+    function registerEngineUpdate(name, updateFunction) {
+        if (engineCallbacks.hasOwnProperty(name)) {
+            throw new Error(`Engine callback with name '${name}' already exists.`);
+        }
+        engineCallbacks[name] = updateFunction;
+    }
+
     function unregisterRaf(name) {
         delete rafCallbacks[name];
     }
 
     function setupRAF() {
-        registerRaf("playerUpdate", update);
+        // registerRaf("playerUpdate", update);
         const rafLoop = function() {
-            if (lastTime === null) {
-                lastTime = performanceNow();
+            if (lastRafTime === null) {
+                lastRafTime = performanceNow();
             }
-            const elapsed = performanceNow() - lastTime;
+            const elapsed = performanceNow() - lastRafTime;
             for (const rafName in rafCallbacks) {
                 const raf = rafCallbacks[rafName];
                 raf(elapsed);
@@ -62,27 +71,42 @@ export function Player(f) {
     }
 
     function setupInput() {
-        addEventListeners(keysPressed, { addEventListener });
         const keyBindings = getDefaultKeyBindings({
             moveLeft,
             moveRight,
             moveUp,
             moveDown
         });
-        const inputLoop = function() {
+        const inputHandler = function(keyPressedOrReleased) {
             handleKeyBindings(keyBindings, keysPressed, lastKeysPressed);
             lastKeysPressed = { ...keysPressed};
+        };
+        addEventListeners(keysPressed, inputHandler, { addEventListener });
+    };
+
+    function setupEngine() {
+        registerEngineUpdate("playerUpdate", update);
+        const engineLoop = function(keyAddedOrRemoved) {
+            if (lastRafTime === null) {
+                lastRafTime = performanceNow();
+            }
+            const elapsed = performanceNow() - lastRafTime;
+            for (const engineName in engineCallbacks) {
+                const engineFunction = engineCallbacks[engineName];
+                engineFunction(elapsed);
+            }
             if (running) {
-                setTimeout(inputLoop, state.gameSpeedInMSPerTick);
+                setTimeout(engineLoop, state.gameSpeedInMSPerTick);
             }
         };
-        setTimeout(inputLoop, state.gameSpeedInMSPerTick);
-    };
+        setTimeout(engineLoop, state.gameSpeedInMSPerTick);
+    }
 
     function start() {
         running = true;
         setupRAF();
         setupInput();
+        setupEngine();
     }
 
     function stop() {
@@ -94,17 +118,21 @@ export function Player(f) {
     // TODO: refactor
     function moveLeft(keyPressed) {
         state.movementVector.x = keyPressed ? -1 : 0;
+        // state.movementVector.y = 0;
     }
 
     function moveRight(keyPressed) {
         state.movementVector.x = keyPressed ? 1 : 0;
+        // state.movementVector.y = 0;
     }
 
     function moveUp(keyPressed) {
+        // state.movementVector.x = 0;
         state.movementVector.y = keyPressed ? -1 : 0;
     }
 
     function moveDown(keyPressed) {
+        // state.movementVector.x = 0;
         state.movementVector.y = keyPressed ? 1 : 0;
     }
 
--- mantary/src/functions/game/addEventListeners.js
+++ mantary/src/functions/game/addEventListeners.js
@@ -1,10 +1,12 @@
-export function addEventListeners(keysPressed, { addEventListener }) {
+export function addEventListeners(keysPressed, updateCallback, { addEventListener }) {
     addEventListener('keydown', function (event) {
         keysPressed[event.key] = true;
+        updateCallback(true);
         // console.log(event.key);
     });
 
     addEventListener('keyup', function (event) {
         delete keysPressed[event.key];
+        updateCallback(false);
     });
 }

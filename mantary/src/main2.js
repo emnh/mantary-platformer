e88785f85a9863324e83a56bdfe7ed791cba4f84
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-16 11:52:07 +0200

    start on keys

--- mantary/src/main2.js
+++ mantary/src/main2.js
@@ -4,13 +4,48 @@
 import { testCollision } from './functions/checkCollision.js';
 import { drawDivs } from './functions/drawDivs.js';
 import { report } from './functions/report.js';
+import { docReady } from './functions/docReady.js';
+import { runTests } from './orchestrations/runTests.js';
 
-drawDivs();
-
-function runTests() {
-    testCollision();
-    report("Two divs should be colliding", true);
-    report("Two divs should be colliding", false)
+const keyBindings = {
+    'a': moveLeft,
+    'd': moveRight,
+    'w': moveUp,
+    's': moveDown,
+    'A': moveLeft,
+    'D': moveRight,
+    'W': moveUp,
+    'S': moveDown,
+    'ArrowLeft': moveLeft,
+    'ArrowRight': moveRight,
+    'ArrowUp': moveUp,
+    'ArrowDown': moveDown,
 };
 
-runTests();
+function addEventListeners(keysPressed) {
+    document.addEventListener('keydown', function (event) {
+        keysPressed[event.key] = true;
+        // console.log(event.key);
+    });
+
+    document.addEventListener('keyup', function (event) {
+        delete keysPressed[event.key];
+    });
+}
+
+function game() {
+
+};
+
+
+
+function main() {
+    const urlParams = new URLSearchParams(window.location.hash.substring(1));
+    if (urlParams.has('test')) {
+        runTests();
+    } else {
+        game();
+    }
+}
+
+docReady(main);

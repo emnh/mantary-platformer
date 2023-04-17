b78d9787320655529b2bbe30f2043a68a69a4003
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-17 11:15:12 +0200

    relative audio paths

--- mantary/src/components/Audio.js
+++ mantary/src/components/Audio.js
@@ -14,13 +14,13 @@ export function Audio(f, musicEnabled) {
 
     function start() {
         coinSound = new Howl({
-            src: [ '/audio/juhani_junkala_chiptune_adventures_1_stage_1.ogg'],
+            src: [ './audio/juhani_junkala_chiptune_adventures_1_stage_1.ogg'],
             autoplay: musicEnabled,
             loop: true,
             volume: 0.25,
         });
         coinSound = new Howl({
-            src: [ '/audio/coin10.wav']
+            src: [ './audio/coin10.wav']
         });
         registerConsumeCoinCallback(coinConsumed);
     }
